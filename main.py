795be05b52bf531c4578015543fadd451133c8ce
Ryan <ryan.collins@ksc.keene.edu>
2020-03-01 23:08:12 -0500

    Added handler for passing off recipe requests by ingredient to the Spoonacular API with a simple caching mechanism

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 import requests
 
 app = Flask(__name__)
 api = Api(app)
 
-class IsAlive(Resource):
+cache = {}
+
+class RecipeByIngredientSimpleCache(Resource):
+    """Returns recipes based on provided ingredients using the Spoonacular API, 
+       using a simple caching mechanism to avoid multiple API calls with the same argument
+    """
     def __init__(self):
         self.key = os.environ['SPOON_API_KEY']
 
     def get(self):
-        return requests.get('https://api.spoonacular.com/recipes/findByIngredients', params={'ingredients': 'shrimp,pasta', 'apiKey': self.key}).json()
+        if request.args.get('ingredients'):
+            ingredients = frozenset({ingr for ingr in request.args['ingredients'].split(',')})
+        else: 
+            return {}
+        if cache.get(ingredients):
+            return cache[ingredients]
+        else:
+            recipes = requests.get('https://api.spoonacular.com/recipes/findByIngredients', params={'ingredients': ingredients, 'apiKey': self.key}).json()
+            cache[ingredients] = recipes
+            return recipes
 
-api.add_resource(IsAlive, '/')
+api.add_resource(RecipeByIngredientSimpleCache, '/')
 
 if __name__ == '__main__':
     app.run()
